ebdf50212bd57319012003632692264491bae7a9
pipi-shortstocking <jannie435@naver.com>
2025-08-31 18:29:51 +0900

    added view

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,8 @@ from django.contrib import admin
 from django.urls import path
 from app import views as app_views
 
-urlpatterns = [path("admin/", admin.site.urls), path("app/", app_views.index)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("app/", app_views.index),
+    path("app/<int:pk>/", app_views.post_detail),
+]
