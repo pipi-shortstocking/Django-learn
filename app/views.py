52faad6cfbac52e092c6dca6604a5cb5f7618805
pipi-shortstocking <jannie435@naver.com>
2025-08-31 18:09:22 +0900

    add app

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from app.models import Post
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    return render(request, "app/index.html", {"post_list": qs})
