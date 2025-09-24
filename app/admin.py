f877fcffb2808c23d2ebc5255231f307f6e73cb7
pipi-shortstocking <jannie435@naver.com>
2025-09-24 18:25:47 +0900

    init

--- app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from app.models import Post
-
-
-admin.site.register(Post)
--- app/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=100)  # 길이 제한 있음
-    content = models.TextField()  # 길이 제한 없음
