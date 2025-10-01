9d490c1204c863725d03210615948320de2c7710
pipi-shortstocking <jannie435@naver.com>
2025-10-01 16:17:53 +0900

    DRF 프로젝트 구조 살펴보기

--- myweb/settings.py
+++ myweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8fgtmsvwf_^!-g%%j-6i654j0l39s-qj!gln2-mk4#u=)x9t2@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
