ab46d7c2dda096fbf952f9b4d976338f5555c6a9
pipi-shortstocking <jannie435@naver.com>
2025-10-04 23:48:52 +0900

    DRF 심화 개념 보충하기

--- example/urls.py
+++ example/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
-from .views import helloAPI, booksAPI, bookAPI, BooksAPI, BookAPI
+from .views import BookAPIGenerics, helloAPI, booksAPI, bookAPI, BooksAPI, BookAPI,BooksAPIMixins, BookAPIMixins, BooksAPIGenerics, BookAPIGenerics, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('books', BookViewSet)
 
 urlpatterns = [
     path('hello/', helloAPI),
@@ -7,4 +11,9 @@ urlpatterns = [
     path('fbv/book/<int:bid>/', bookAPI), # 함수형 뷰의 booksAPI 연결
     path('cbv/books/', BooksAPI.as_view()),
     path('cbv/book/<int:bid>/', BookAPI.as_view()), # 클래스형 뷰의 BooksAPI 연결
+    path('mixin/books/', BooksAPIMixins.as_view()),
+    path('mixin/book/<int:bid>/', BookAPIMixins.as_view()),
+    path('generics/books/', BooksAPIGenerics.as_view()),
+    path('generics/book/<int:bid>/', BookAPIGenerics.as_view()),
+    path('viewset/', include(router.urls)),
 ]
--- example/views.py
+++ example/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, generics, status
+from rest_framework import viewsets, permissions, generics, status, mixins
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -51,3 +51,38 @@ class BookAPI(APIView):
         book = get_object_or_404(Book, bid=bid)
         serializer = BookSerializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self, request, *args, **kwargs): # GET 메소드 처리 함수(전체 목록)
+        return self.list(request, *args, **kwargs) # mixins.ListModelMixin과 연결
+    def post(self, request, *args, **kwargs): # POST 메소드 처리 함수(1권 등록)
+        return self.create(request, *args, **kwargs) # mixins.CreateModelmixin과 연결
+
+class BookAPIMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid'
+    # 우리는 Django 기본 모델 pk가 아닌 bid를 pk로 사용하고 있으니 lookup_field로 설정
+
+    def get(self, request, *args, **kwargs): # GET 메소드 처리 함수(1권)
+        return self.retrieve(request, *args, **kwargs) # mixins.RetrieveModelMixin과 연결
+    def put(self, request, *args, **kwargs): # PUT 메소드 처리 함수(1권 수정)
+        return self.update(request, *args, **kwargs) # mixins.UpdateModelMixin과 연결
+    def delete(self, request, *args, **kwargs): # DELETE 메소드 처리 함수(1권 삭제)
+        return self.destroy(request, *args, **kwargs) # mixins.DestroyModelMixin과 연결
+
+class BooksAPIGenerics(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookAPIGenerics(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid'
+
+class BookViewSet(viewsets.ModelViewSet): # 라우터 지정 필요
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
