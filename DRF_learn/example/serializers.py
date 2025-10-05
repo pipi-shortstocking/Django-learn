5b961971372b2c1811ef5fc290a02bd034f565a1
pipi-shortstocking <jannie435@naver.com>
2025-10-05 17:21:24 +0900

    remove squashed subtree DRF_learn

--- DRF_learn/example/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Book
-
-class BookSerializer(serializers.ModelSerializer): # 모델의 내용을 기반으로 동작하는 시리얼라이저 -> 코드 중복을 줄임
-    class Meta:
-        model = Book
-        fields = ['bid', 'title', 'author', 'category', 'pages', 'price', 'published_date', 'description']
