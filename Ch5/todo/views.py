b3f5e1f7d13a3bab97058a93b3a0ea47651d756d
pipi-shortstocking <jannie435@naver.com>
2025-10-29 16:01:27 +0900

    Todo 생성 API 만들기

--- Ch5/todo/views.py
+++ Ch5/todo/views.py
@@ -6,13 +6,20 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 
 from .models import Todo
-from .serializers import TodoSimpleSerializer, TodoDetailSerializer
+from .serializers import TodoCreateSerializer, TodoSimpleSerializer, TodoDetailSerializer
 
 class TodosAPIView(APIView):
     def get(self, request):
         todos = Todo.objects.filter(complete=False)
         serializer = TodoSimpleSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = TodoCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TodoAPIView(APIView):
     def get(self, request, pk):
