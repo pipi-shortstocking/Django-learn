e77b2258d704c9ee10a84d3edb1a4d9eefe32dbb
pipi-shortstocking <jannie435@naver.com>
2025-11-12 19:27:04 +0900

    페이지네이션

--- Ch6/myboard/settings.py
+++ Ch6/myboard/settings.py
@@ -108,7 +108,14 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':
+    3,
 }
 
 STATIC_URL = '/static/'
